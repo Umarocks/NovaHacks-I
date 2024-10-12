d6315d2364822a5a93fe232f6071025d817bf2e8
notnotmelon <zacharypicco@gmail.com>
2024-10-12 17:53:18 -0500

    refactor: Move model code into model file

--- app/models/langchain_model.py
+++ app/models/langchain_model.py
@@ -1,28 +1,67 @@
 # app/models/langchain_model.py
 
 import pandas as pd
-from langchain_experimental.agents import create_pandas_dataframe_agent
-from langchain.llms import OpenAI
-from openai_api_key import get_keys
 
 # Function to load the CSV data and create the LangChain agent
-def create_langchain_agent():
-    # Load the dataset into a pandas DataFrame
-    data_frame = pd.read_csv("./Datasets/owid-energy-data.csv")
+from langchain_experimental.agents import create_pandas_dataframe_agent
+from langchain.llms import OpenAI
+from app.services.openai_api_key import get_keys
 
-    # Drop certain columns
+def read_csv_into_dataframe(csv_name):
+   df = pd.read_csv(csv_name)
+   return df
+
+def get_owid_dataframe():
+    continents_to_drop = [
+        'Africa',
+        'Asia',
+        'Central African Republic',
+        'Central America',
+        'Central Asia',
+        'Eastern Africa',
+        'Eastern Asia',
+        'Eastern Europe',
+        'Europe',
+        'European Union',
+        'Latin America',
+        'Middle Africa',
+        'North America',
+        'Northern Africa',
+        'Northern Europe',
+        'Oceania',
+        'South America',
+        'South-Eastern Asia',
+        'Southern Africa',
+        'Southern Asia',
+        'Southern Europe',
+        'Western Africa',
+        'Western Asia',
+        'Western Europe',
+        'World',
+    ]
+
+    data_frame = read_csv_into_dataframe("./Datasets/owid-energy-data.csv")
+
+    data_frame = data_frame[~data_frame['Country'].isin(continents_to_drop)]
+    data_frame = data_frame[~data_frame['Country'].str.endswith(' (EIA)')]
+    data_frame = data_frame[~data_frame['Country'].str.endswith(' (EI)')]
+    data_frame = data_frame[~data_frame['Country'].str.endswith(' (Shift)')]
+    data_frame = data_frame[~data_frame['Country'].str.endswith(' (Ember)')]
+    data_frame = data_frame[~data_frame['Country'].str.contains('-income countries')]
+    data_frame = data_frame[~data_frame['Country'].str.contains('Europe')]
     data_frame = data_frame.drop(columns=[col for col in data_frame.columns if col.endswith('_per_capita')])
+    return data_frame
 
-    # Initialize OpenAI LLM
-    llm = OpenAI(temperature=0.5, openai_api_key=get_keys())
+def create_pandas_agent():
+    data_frame = get_owid_dataframe()
 
-    # Create the LangChain agent with the dataframe and LLM
-    p_agent = create_pandas_dataframe_agent(
-        handle_parsing_errors=True,
-        allow_dangerous_code=True,
-        llm=llm,
-        df=data_frame,
-        verbose=True
+    llm = OpenAI(temperature=0.7, openai_api_key = get_keys())
+
+    return create_pandas_dataframe_agent(
+        handle_parsing_errors = True,
+        allow_dangerous_code = True,
+        llm = llm,
+        df = data_frame,
+        verbose = True,
+        return_intermediate_steps = True
     )
-    
-    return p_agent
