0c353d5bc278fb3ef00e9830044cedb03ecfa4a4
Umarocks <umarocks@hotmail.com>
2024-10-17 12:54:22 -0500

    LLM prompt improvements

--- app/services/langchain_service.py
+++ app/services/langchain_service.py
@@ -14,11 +14,11 @@ class LLM:
     def query(self, query):
         try:
             lower_query = query.lower()
-            if "per capita" in lower_query:
-                query = f"{query} Make sure to divide the result by the population of the country."
-            else:
-                query = f"{query} If the user asks for something unreleated, please respond \"I cannot help with that.\""
-            query = f"{query} Explain your reasoning to the user without mentioning the use of a dataframe."
+            # if "per capita" in lower_query:
+            #     query = f"{query} Make sure to divide the result by the population of the country."
+            # else:
+            #     query = f"{query} If the user asks for something unreleated, please respond \"I cannot help with that.\""
+            # query = f"{query} Explain your reasoning to the user without mentioning the use of a dataframe."
             return self.p_agent.invoke(query)
         except Exception as e:
             print(e)
